16ddbf9b9d9415c94bf26758e7feadbd7cee4022
Tisdale Fry <tisdalefry@gmail.com>
2020-03-04 18:24:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,8 +35,8 @@ def map_to_square(source_array)
   sqr_arr
 end
 
-def reduce_to_total(source_array)
- count = 0
+def reduce_to_total(source_array, starting_point = 0)
+ count = starting_point
  idx = 0
  while idx < source_array.length do
    count += source_array[idx]
