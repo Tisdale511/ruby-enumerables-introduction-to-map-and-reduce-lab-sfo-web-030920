44ca40b57e3a66c78570b61e23b6f8f568fbd752
Tisdale Fry <tisdalefry@gmail.com>
2020-03-04 17:52:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,8 +46,12 @@ def reduce_to_total(source_array)
 end
 
 def reduce_to_all_true(source_array)
-  binding.pry
-  
+  idx = 0
+  while idx < source_array.length do
+    source_array[idx] = true
+    idx += 1
+  end
+  source_array
 end
 
 
