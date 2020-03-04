4fa8f89e5d306d23751dbc2105f065f92adefda1
Tisdale Fry <tisdalefry@gmail.com>
2020-03-04 17:47:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,56 @@
 # My Code here....
+require 'pry'
+
+def map_to_negativize(source_array)
+  idx = 0
+  neg_arr = []
+  while  idx < source_array.length do
+    neg_arr << source_array[idx]*(-1)
+    idx += 1
+  end
+  neg_arr
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  doub_arr = []
+  idx = 0
+  while idx < source_array.length
+    doub_arr << source_array[idx]*2  
+    idx += 1
+  end
+  doub_arr
+end
+
+def map_to_square(source_array)
+  sqr_arr = []
+  idx = 0
+  while idx < source_array.length do
+    sqr_arr << source_array[idx]**2
+    idx += 1
+  end
+  sqr_arr
+end
+
+def reduce_to_total(source_array)
+ count = starting_point
+ idx = 0
+ while idx < source_array.length do
+   starting_point += source_array[idx]
+   idx += 1
+ end
+ 
+end
+
+
+
+
+
+
+
+
+
+
